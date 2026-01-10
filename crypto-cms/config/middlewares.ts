fca2f4e53ce735f22922361375e502f9f5feaf3c
Marko <icyphotoget@gmail.com>
2026-01-10 21:22:01 +0100

    fix: allow R2 images in Strapi admin CSP

--- crypto-cms/config/middlewares.ts
+++ crypto-cms/config/middlewares.ts
@@ -1,12 +1,35 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://*.r2.cloudflarestorage.com",
+            // ako kasnije koristiš custom CDN domen:
+            // "https://cdn.tvojdomain.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://*.r2.cloudflarestorage.com",
+          ],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
