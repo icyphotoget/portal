e5ff030a52463e8f561f6edfd070b194566fee42
Marko <icyphotoget@gmail.com>
2026-01-10 19:14:15 +0100

    chore(cms): add R2 upload provider config

--- crypto-cms/config/plugins.ts
+++ crypto-cms/config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-aws-s3',
+      providerOptions: {
+        s3Options: {
+          region: 'auto',
+          endpoint: env('R2_ENDPOINT'),
+          credentials: {
+            accessKeyId: env('R2_ACCESS_KEY_ID'),
+            secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+          },
+        },
+        params: {
+          Bucket: env('R2_BUCKET'),
+        },
+      },
+    },
+  },
+});
